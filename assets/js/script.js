66a33bb2f6e28cf86018805a89935274defe3589
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-12-16 21:13:58 -0500

    configured moment js for due date audits

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,9 @@ var createTask = function (taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
+
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -39,6 +42,28 @@ var loadTasks = function () {
 
 var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+var auditTask = function (taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  console.log(date);
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  console.log(time);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
 };
 
 // enable draggable/sortable feature on list-group elements
@@ -103,6 +128,12 @@ $("#trash").droppable({
   },
 });
 
+// convert text field into a jquery date picker
+$("#modalDueDate").datepicker({
+  // force user to select a future date
+  minDate: 1,
+});
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function () {
   // clear values
@@ -155,7 +186,7 @@ $(".list-group").on("blur", "textarea", function () {
   // get the textarea's current value/text
   var text = $(this).val();
 
-  // get the parent ul's id attribute
+  // get status type and position in the list
   var status = $(this).closest("list-group").attr("id").replace("list-", "");
   //get the task's position in the list of other li elements
   var index = $(this).closest("list-group-item").index();
@@ -167,7 +198,7 @@ $(".list-group").on("blur", "textarea", function () {
   // recreate p element with new textarea
   var taskP = $("<p>").addClass("m-1").text(text);
 
-  // replace textarea with P element
+  // replace textarea with P element (new content)
   $(this).replaceWith(taskP);
 });
 
@@ -217,11 +248,7 @@ $(".list-group").on("change", "input[type='text']", function () {
     .text(date);
   // replace input with span element
   $(this).replaceWith(taskSpan);
-});
-
-// Modal due date
-$("#modalDueDate").datepicker({
-  minDate: 1,
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // remove all tasks
